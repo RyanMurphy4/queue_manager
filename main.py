205b3769756cf86cad74f5cdd6d6cda6e19bdc85
DeontayPeterson <108695175+DeontayPeterson@users.noreply.github.com>
2022-09-09 18:36:02 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,29 +5,32 @@ import win32gui, win32ui, win32con
 import time
 import torch
 
-# reader = easyocr.Reader(['en'], gpu=torch.cuda.is_available())
+
 reader = easyocr.Reader(['en'], gpu=torch.cuda.is_available())
 
-user_screen = Screencap()
-user_screen.update_res_and_window_points()
+screen = Screencap()
+screen.update_res_and_window_points()
 
 hwnd = win32gui.FindWindow(None, 'War  ')
 win = win32ui.CreateWindowFromHandle(hwnd)
 
-in_queue = True
+##TODO: Monitor your CURRENT position in queue. Maybe just parse 'result'?
 
+
+in_queue = True
 while in_queue:
-    queue_position, travel_position = user_screen.get_two_cropped_img()
+    queue_position, travel_position = screen.get_two_cropped_img()
 
     result = reader.readtext(queue_position, detail=0, paragraph=True)
     result2 = reader.readtext(travel_position, detail=0, paragraph=True)
     print(f"Queue Position: {result}")
     print(f"Travel Position: {result2}")
 
-    if result2:
-        if result2[0] == 'Press E to travel to adjacent region 21231':
+    if result2: #Avoid exception handling. Only check the text, if text is detected.
+        if result2[0] == 'Press E to travel to adjacent region':
             win.SendMessage(win32con.WM_KEYDOWN, 0x45, 0)
             time.sleep(.01)
+            win.SendMessage(win32con.WM_KEYUP, 0x45, 0)
             in_queue = False
 
     cv.imshow('Queue Position', queue_position)
@@ -38,3 +41,6 @@ while in_queue:
     if waitkey == ord('q'):
         cv.destroyAllWindows()
 
+
+
+##TODO: Test at as many borders as possible.
